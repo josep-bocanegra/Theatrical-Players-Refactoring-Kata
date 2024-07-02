c23ee9e40ee2abb6534ec168e4ff35a8be882bcd
josep-bocanegra <40354250+josep-bocanegra@users.noreply.github.com>
2024-07-02 12:05:24 +0200

    r split loop

--- csharp/TheatricalPlayersRefactoringKata/StatementPrinter.cs
+++ csharp/TheatricalPlayersRefactoringKata/StatementPrinter.cs
@@ -17,14 +17,18 @@ namespace TheatricalPlayersRefactoringKata
             {
                 var play = plays[perf.PlayID];
                 var thisAmount = play.GetPlayAmount(perf.Audience);
-                // add volume credits
-                volumeCredits += Math.Max(perf.Audience - 30, 0);
-                // add extra credit for every ten comedy attendees
-                if ("comedy" == play.Type) volumeCredits += (int)Math.Floor((decimal)perf.Audience / 5);
-
                 // print line for this order
                 result += String.Format(cultureInfo, "  {0}: {1:C} ({2} seats)\n", play.Name, Convert.ToDecimal(thisAmount / 100), perf.Audience);
                 totalAmount += thisAmount;
+            }
+            foreach(var perf in invoice.Performances) 
+            {
+                var play = plays[perf.PlayID]; 
+                // add volume credits
+                var volumeCreditPlay = Math.Max(perf.Audience - 30, 0);
+                // add extra credit for every ten comedy attendees
+                if ("comedy" == play.Type) volumeCreditPlay += (int)Math.Floor((decimal)perf.Audience / 5);
+                volumeCredits += volumeCreditPlay;
             }
             result += String.Format(cultureInfo, "Amount owed is {0:C}\n", Convert.ToDecimal(totalAmount / 100));
             result += String.Format("You earned {0} credits\n", volumeCredits);
