070ca61754a803ba98e30f9aacaec43916f393c5
josep-bocanegra <40354250+josep-bocanegra@users.noreply.github.com>
2024-07-02 12:47:42 +0200

    r create print method

--- csharp/TheatricalPlayersRefactoringKata/StatementPrinter.cs
+++ csharp/TheatricalPlayersRefactoringKata/StatementPrinter.cs
@@ -12,18 +12,28 @@ namespace TheatricalPlayersRefactoringKata
             var result = string.Format("Statement for {0}\n", invoice.Customer);
             CultureInfo cultureInfo = new CultureInfo("en-US");
 
+            var resultPlay = "";
             foreach(var perf in invoice.Performances) 
             {
                 var play = plays[perf.PlayID];
                 var thisAmount = play.GetPlayAmount(perf.Audience);
                 // print line for this order
-                result += String.Format(cultureInfo, "  {0}: {1:C} ({2} seats)\n", play.Name, Convert.ToDecimal(thisAmount / 100), perf.Audience);
+                resultPlay += String.Format(cultureInfo, "  {0}: {1:C} ({2} seats)\n", play.Name, Convert.ToDecimal(thisAmount / 100), perf.Audience);
             }
+
+            result += resultPlay;
             var totalAmount = invoice.TotalAmount(plays);
             var volumeCredits = invoice.VolumeCredits(plays);
-            result += String.Format(cultureInfo, "Amount owed is {0:C}\n", Convert.ToDecimal(totalAmount / 100));
-            result += String.Format("You earned {0} credits\n", volumeCredits);
+            
+       
+            result += PrintVolumeCredit(volumeCredits, totalAmount);
             return result;
+        }
+
+        private static string PrintVolumeCredit(int volumeCredits, int totalAmount)
+        {
+            CultureInfo cultureInfo = new CultureInfo("en-US");
+            return String.Format(cultureInfo, "Amount owed is {0:C}\n", Convert.ToDecimal(totalAmount / 100)) + String.Format("You earned {0} credits\n", volumeCredits);
         }
     }
 }
